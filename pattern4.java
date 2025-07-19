6e45490e228b273014b4af3b89b5b278dcdde7a5
arunraj jayakumar <162810710+arunraj-3117@users.noreply.github.com>
2025-07-19 22:05:47 +0530

    Add files via upload

---
+++ pattern4.java
@@ -0,0 +1,27 @@
+//PATTERN 4
+/*
+* * * * *
+*       *
+*       *
+*       *
+* * * * *
+
+*/
+import java.util.Scanner;
+
+public class pattern4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); 
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || i == n - 1 || j == 0 || j == n - 1) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  "); 
+                }
+            }
+            System.out.println();
+        }
+    }
+}
