1100302240db8756f36bc630f56294d05395ce17
arunraj jayakumar <162810710+arunraj-3117@users.noreply.github.com>
2025-08-11 23:04:21 +0530

    Add files via upload

---
+++ pattern25.java
@@ -0,0 +1,25 @@
+//PATTERN 25
+/*
+*   *
+ * * 
+  *  
+ * * 
+*   *
+*/
+import java.util.*;
+public class pattern25 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt(); 
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j || i + j == n - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
