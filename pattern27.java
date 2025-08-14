a754209d537addb16cdc7f10c94bef1d6801e1e2
arunraj jayakumar <162810710+arunraj-3117@users.noreply.github.com>
2025-08-14 22:27:26 +0530

    Add files via upload

---
+++ pattern27.java
@@ -0,0 +1,27 @@
+//PATTERN27
+/*
+* * * * *
+  * 
+    *
+      *
+* * * * *
+*/
+import java.util.*;
+public class pattern27{
+    public static void main(String[] args ){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the value n= ");
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 || i==n-1 || i==j){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
